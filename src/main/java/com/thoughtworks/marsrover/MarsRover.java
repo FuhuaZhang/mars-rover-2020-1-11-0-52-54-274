8594cfb3b8ae0027bddcd7c8a6f678cf48c00d75
Fuhua Zhang <zhangfuhua@hipagesgroup.com.au>
2020-01-14 22:50:15 +0800

    feat: return position after move of turn

--- src/main/java/com/thoughtworks/marsrover/MarsRover.java
+++ src/main/java/com/thoughtworks/marsrover/MarsRover.java
@@ -14,11 +14,97 @@ class MarsRoverPos {
     enum MarsRoverDirection {
         N, S, W, E,
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof MarsRoverPos){
+            MarsRoverPos marsRoverPos = (MarsRoverPos) obj;
+            return this.x == marsRoverPos.x && this.y == marsRoverPos.y && this.direction == marsRoverPos.direction;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
+
+enum MoveOrTurn {
+    M, L, R,
 }
 
 public class MarsRover {
 
-    public MarsRoverPos explore(MarsRoverPos marsRoverPos) {
+    public MarsRoverPos explore(MarsRoverPos marsRoverPos, MoveOrTurn[] moveOrTurns) {
+        MarsRoverPos result = marsRoverPos;
+        for (MoveOrTurn moveOrTurn : moveOrTurns) {
+            switch (moveOrTurn) {
+                case M:
+                    result = handleMove(marsRoverPos);
+                    break;
+                case R:
+                    result = handleTurnR(marsRoverPos);
+                    break;
+                default:
+                    result = handleTurnL(marsRoverPos);
+                    break;
+            }
+        }
+        return result;
+    }
+
+    MarsRoverPos handleMove(MarsRoverPos marsRoverPos){
+        switch (marsRoverPos.direction){
+            case N:
+                marsRoverPos.y += 1;
+                break;
+            case S:
+                marsRoverPos.y -= 1;
+                break;
+            case E:
+                marsRoverPos.x += 1;
+                break;
+            case W:
+                marsRoverPos.x -= 1;
+                break;
+        }
+        return marsRoverPos;
+    }
+
+    MarsRoverPos handleTurnR(MarsRoverPos marsRoverPos){
+        switch (marsRoverPos.direction){
+            case N:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.E;
+                break;
+            case S:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.W;
+                break;
+            case E:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.S;
+                break;
+            case W:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.N;
+                break;
+        }
+        return marsRoverPos;
+    }
+
+    MarsRoverPos handleTurnL(MarsRoverPos marsRoverPos){
+        switch (marsRoverPos.direction){
+            case N:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.W;
+                break;
+            case S:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.E;
+                break;
+            case E:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.N;
+                break;
+            case W:
+                marsRoverPos.direction = MarsRoverPos.MarsRoverDirection.S;
+                break;
+        }
         return marsRoverPos;
     }
 }
--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -2,6 +2,8 @@ package com.thoughtworks.marsrover;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
@@ -9,7 +11,16 @@ class MarsRoverTest {
     @Test
     void should_return_the_init_position_and_direction_info_given_only_init_order() {
         MarsRoverPos marsRoverPos = new MarsRoverPos(1, 1, MarsRoverPos.MarsRoverDirection.N);
-        MarsRoverPos newMarsRoverPos = marsRover.explore(marsRoverPos);
+        MarsRoverPos newMarsRoverPos = marsRover.explore(marsRoverPos, new MoveOrTurn[]{});
         assertEquals(marsRoverPos, newMarsRoverPos);
+    }
+
+    @Test
+    void should_return_the_position_after_move() {
+        MarsRoverPos marsRoverPos = new MarsRoverPos(1, 1, MarsRoverPos.MarsRoverDirection.N);
+        MoveOrTurn[] moveOrTurns = {MoveOrTurn.M};
+        MarsRoverPos newMarsRoverPos = marsRover.explore(marsRoverPos, moveOrTurns);
+        MarsRoverPos expect = new MarsRoverPos(1, 2, MarsRoverPos.MarsRoverDirection.N);
+        assertSame(expect, newMarsRoverPos);
     }
 }
